829cf23789689252ad66ff2ccc6b52ee3535cfe6
Vlad Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2019-01-31 00:25:33 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function activate(){
+  const ul1 = document.getElementById('ul1').style.display = "flex";
+  const ul2 = document.getElementById('ul2').style.display = "flex";
+  const login = document.getElementById('login').style.display = "none";
+  const container = document.getElementById('container').style.backgroundImage = "url('./images/space-bg.jpg')";
+  const mainImg = document.getElementById('mainImg').src = "./images/max.png";
+
+  const music = document.getElementById('music').play();
+}
